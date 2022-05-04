312e41efc712414d23bac80e9dca4626a4696836
Yoshita <Yoshita@192.168.1.10>
2022-05-04 21:14:46 +0530

    5555555

---
+++ src/Moore/A54_program.java
@@ -0,0 +1,19 @@
+package Moore;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class A54_program {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
