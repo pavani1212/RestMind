6bdfac82dd9473d4b226db369cc1a3b9a100ce66
Yoshita <Yoshita@192.168.1.10>
2022-05-04 21:11:14 +0530

    444444

---
+++ src/Moore/Class.java
@@ -0,0 +1,19 @@
+package Moore;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class Class {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+}
